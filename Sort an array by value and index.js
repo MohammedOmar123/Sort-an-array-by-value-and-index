2d681ea98d4253c98aaf1bef09e92c8bb70dcd9b
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-07-07 06:48:32 +0300

    ss

---
+++ Sort an array by value and index.js
@@ -0,0 +1,24 @@
+// let array = [ 5, 20, -29, -27, -21, 24, -1,   5,  -7, -29];
+// let max = 0;
+
+// let res = [];
+
+// for (let i = 0; i < array.length; i++) {
+//   res[i] = array[i] * (i + 1);
+// }
+// console.log(res);
+
+// for (let i = 0; i < res.length; i++) {
+//   for (let j = 0; j < array.length; j++) {
+//     if (res[i] >= res[j]) {
+//       let swap = res[i];
+//       res[i] = res[j];
+//       res[j] = swap
+//       swap = array[i];
+//       array[i] = array[j];
+//       array[j] = swap ;
+//     }
+//   }
+// }
+// console.log(array.reverse());
+
